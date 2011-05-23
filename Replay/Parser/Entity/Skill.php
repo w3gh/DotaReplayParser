cab17f142fae80210477ddfa2b5d208cac093f1e
JiLiZART <JiLiZART@Gmail.com>
2011-05-23 23:24:25 +0700

    First commit

---
+++ Replay/Parser/Entity/Skill.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * @author Nikolay Kosturin <jilizart@gmail.com>
+ * @author Seven <zabkar@gmail.com>
+ * @copyright Copyright &copy; 2010-2012 Nikolay Kostyurin, Seven, Julas, Rush4Hire, esby and Rachmadi
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+/**
+ * @see Dota_Replay_Parser_Entity_Abstract
+ */
+require_once 'Dota/Replay/Parser/Entity/Abstract.php';
+
+/**
+ * Description of Skill
+ *
+ * @author Nikolay Kosturin <jilizart@gmail.com>
+ * @version $Id:$
+ * @package 
+ */
+class Dota_Replay_Parser_Entity_Skill extends Dota_Replay_Parser_Entity_Abstract
+{
+  public function  __construct($Name, $Art, $Comment, $Cost, $Id, $ProperNames, $RelatedTo, $Type)
+  {
+    parent::__construct($Name, $Art, $Comment, $Cost, $Id, $ProperNames, $RelatedTo, $Type);
+  }
+}
+
